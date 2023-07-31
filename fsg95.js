de47e32809058120a42b0e31da11c8664f9645ec
David <40262439+quzyp@users.noreply.github.com>
2023-07-31 21:52:49 +0200

    Add files via upload

---
+++ fsg95.js
@@ -0,0 +1,59 @@
+$( document ).ready(function() {
+    var solution = "code"
+    var lenAnswer = solution.length;
+    var solved = false;
+
+    var inputbox = '<input type="text" size="1" maxlength="1" class="empty">'
+    $("#userinput").append(inputbox);
+    $("#userinput").append(inputbox); 
+    $("#userinput").append(inputbox); 
+
+    $("input[type=text]").each(function( index ) {
+        if (index == 0) {
+            $(this).focus();
+            $(this).select(); 
+        }
+    });
+
+    $("input").keyup(function () {
+        var answer = ""
+
+        if (this.value.length == this.maxLength) {
+        $(this).next('input').focus();
+        $(this).next('input').select();
+        }
+            
+        $("input[type=text]").each(function( index ) {
+            if ($(this).val().length > 0) {
+                $(this).attr('class', 'filled');
+                answer = answer + $(this).val();
+            }
+            else {
+                $(this).attr('class', 'empty');
+            }
+        });
+        
+        if (answer.length == lenAnswer) {
+            if (answer.toLowerCase() == solution) {
+                solved = true;
+                $("input[type=text]").each(function( index ) {
+                    $(this).attr('class', 'solved');
+                });      
+                $(".next").removeAttr('hidden');
+            };
+        };
+    });
+
+
+    $(".reset").click(function() {
+        $("input[type=text]").each(function( index ) {
+            $(this).val("");
+            $(this).attr('class', 'empty');
+            if (index == 0) {
+                $(this).focus();
+                $(this).select(); 
+            }
+        });
+    });
+
+});
